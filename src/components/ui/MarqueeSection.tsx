5bb7f6475afd1098880ebaea3c94ee8e0752875e
yuyyuyswallowtail <yuyyuyswallowtail@gmail.com>
2026-02-16 09:31:54 +0700

    refactor:fix text size marquee

--- src/components/ui/MarqueeSection.tsx
+++ src/components/ui/MarqueeSection.tsx
@@ -14,7 +14,7 @@ export default function MarqueeSection({
   return (
     <div className="py-8 lg:py-12 border-y border-foreground bg-background">
       <Marquee direction={direction} speed={40} gradient={false}>
-        <h2 className="text-2xl md:text-4xl lg:text-6xl font-bold mx-12 uppercase tracking-widest overflow-hidden">
+        <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mx-12 uppercase tracking-widest overflow-hidden">
           {title}
         </h2>
       </Marquee>
